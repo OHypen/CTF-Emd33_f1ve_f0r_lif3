092c2b88fa6e684d667c7d2acf85c540bb967ea7
Luis H <goldff36@gmail.com>
2021-04-28 13:11:46 -0400

    0.1
    0.1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Emdee Five for Life
+## Hack The Box: https://app.hackthebox.eu/challenges/67
+
+### 🔨 Was the schedule used?
+    - Yes
+###  What technology was used?
+    - Python
+    - HashLib
+    - Requests
+    
+### 🏆 Victory: https://www.hackthebox.eu/achievement/challenge/579752/67
---
+++ script.py
@@ -0,0 +1,26 @@
+import requests, hashlib
+
+#########################################################
+
+URI =  'http://178.62.116.78:30111/'
+PROXIES = {'http':"http://178.62.116.78:30111/"}
+
+#########################################################
+
+def get_and_hash(ret):
+    begin = ret.find("<h3 align='center'>") + 19
+    end = ret.find("</h3>")
+    md5_string = ret[begin:end].encode('utf-8')
+    digest = hashlib.md5(md5_string).hexdigest()
+    jls_extract_var = digest
+    return jls_extract_var 
+
+##########################################################
+
+session = requests.Session()
+req = session.get(URI, proxies=PROXIES)
+md5 = get_and_hash(req.text)
+req = session.post(URI, data={"hash":md5}, proxies=PROXIES)
+print(req.text)
+
+##########################################################
